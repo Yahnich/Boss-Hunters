a7c669a8acd22067f5f20a4323ded6254fba78d1
Sidearms <gregradi@yahoo.com>
2018-03-02 13:34:32 -0600

    Magnus Adjustments

--- game/scripts/vscripts/heroes/hero_magnus/mag_polarity_2.lua
+++ game/scripts/vscripts/heroes/hero_magnus/mag_polarity_2.lua
@@ -44,10 +44,10 @@ function mag_polarity_2:OnSpellStart()
     EmitSoundOn("Hero_Magnataur.ReversePolarity.Cast", self:GetCaster())
 
     ParticleManager:FireParticle("particles/units/heroes/hero_magnus/magnus_polarity_2.vpcf", PATTACH_POINT, caster, {[0]=Vector(1,0,0), [1]=Vector(radius,radius,radius), [2]=Vector(0.3,0,0), [3]=point})
-    local magnets = caster:FindFriendlyUnitsInRadius(caster:GetAbsOrigin(), radius)
+    local magnets = caster:FindFriendlyUnitsInRadius(point, radius)
     for _,magnet in pairs(magnets) do
         if magnet:HasModifier("modifier_mag_magnet") then
-            ParticleManager:FireParticle("particles/units/heroes/hero_magnus/magnus_polarity_2.vpcf", PATTACH_POINT, magnet, {[0]=Vector(1,0,0), [1]=Vector(radius,radius,radius), [2]=Vector(0.3,0,0), [3]=magnet:GetAbsOrigin()})
+            ParticleManager:FireParticle("particles/units/heroes/hero_magnus/magnus_polarity_2.vpcf", PATTACH_POINT, caster, {[0]=Vector(1,0,0), [1]=Vector(radius,radius,radius), [2]=Vector(0.3,0,0), [3]=magnet:GetAbsOrigin()})
             local enemies = caster:FindEnemyUnitsInRadius(magnet:GetAbsOrigin(), radius, {})
             for _,enemy in pairs(enemies) do
                 ParticleManager:FireRopeParticle("particles/units/heroes/hero_magnataur/magnataur_reverse_polarity_pull.vpcf", PATTACH_POINT, caster, enemy, {[0]=magnet:GetAbsOrigin(), [1]=enemy:GetAbsOrigin()})
