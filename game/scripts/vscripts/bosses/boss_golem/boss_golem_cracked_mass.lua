a11c4364dd5ae8fa24a69be11f8a15c50094158f
Yahnich <stunter_yannick@hotmail.com>
2018-05-03 18:35:10 +0200

    golem boy adjustments

--- game/scripts/vscripts/bosses/boss_golem/boss_golem_cracked_mass.lua
+++ game/scripts/vscripts/bosses/boss_golem/boss_golem_cracked_mass.lua
@@ -12,16 +12,25 @@ function modifier_boss_golem_cracked_mass:DeclareFunctions()
 end
 
 function modifier_boss_golem_cracked_mass:OnTakeDamage(params)
-	if params.unit == self:GetParent() and RollPercentage( self:GetSpecialValueFor("shard_chance") ) then
-		local hp = math.max( params.damage * self:GetSpecialValueFor("shard_damage_mult"), self:GetSpecialValueFor("shard_min_hp"))
-		local scale = self:GetParent():GetModelScale()
-		
-		shardling = CreateUnitByName("npc_dota_boss12_shardling", self:GetParent():GetAbsOrigin() + RandomVector(250), false, nil, nil, self:GetParent():GetTeam())
-		shardling:SetModelScale( math.max(shardling:GetModelScale() * scale, 0.4 ) )
-		Timers:CreateTimer(0.1, function()
+	if params.unit == self:GetParent() and not ( HasBit(params.damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) or HasBit(params.damage_flags, DOTA_DAMAGE_FLAG_REFLECTION) ) then 
+		local chance = math.min( self:GetSpecialValueFor("shard_chance"), math.max(self:GetSpecialValueFor("shard_chance")/3, params.original_damage / params.unit:GetMaxHealth() ) )
+		if RollPercentage( chance ) then
+			local hp = math.max( params.original_damage * self:GetSpecialValueFor("shard_damage_mult"), self:GetSpecialValueFor("shard_min_hp"))
+			local scale = self:GetParent():GetModelScale()
+			
+			shardling = CreateUnitByName("npc_dota_boss12_shardling", self:GetParent():GetAbsOrigin() + RandomVector(250), false, nil, nil, self:GetParent():GetTeam())
+			shardling:SetModelScale( math.max(shardling:GetModelScale() * scale, 0.4 ) )
 			shardling:SetBaseMaxHealth( hp )
 			shardling:SetMaxHealth( hp )
 			shardling:SetHealth( hp )
-		end)
+			print(hp, shardling:GetBaseMaxHealth(), "pretimer")
+			Timers:CreateTimer(0.1, function()
+				
+				shardling:SetBaseMaxHealth( hp )
+				shardling:SetMaxHealth( hp )
+				shardling:SetHealth( hp )
+				print(hp, shardling:GetBaseMaxHealth(), "post timer")
+			end)
+		end
 	end
 end
