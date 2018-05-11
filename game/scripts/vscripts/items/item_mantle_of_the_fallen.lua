4ec21bf3aced495796762ddf9ae80a12dde908e9
Yahnich <stunter_yannick@hotmail.com>
2018-05-11 21:44:23 +0200

    leak and illu fixes

--- game/scripts/vscripts/items/item_mantle_of_the_fallen.lua
+++ game/scripts/vscripts/items/item_mantle_of_the_fallen.lua
@@ -95,7 +95,7 @@ function modifier_item_mantle_of_the_fallen_aura:GetModifierConstantManaRegen()
 end
 
 function modifier_item_mantle_of_the_fallen_aura:OnTakeDamage(params)
-	if params.attacker == self:GetParent() and params.damage_category == DOTA_DAMAGE_CATEGORY_ATTACK then
+	if params.attacker == self:GetParent() and params.damage_category == DOTA_DAMAGE_CATEGORY_ATTACK  and self:GetParent():GetHealth() > 0 and self:GetParent():IsRealHero() and not params.inflictor then
 		local flHeal = params.damage * self.lifesteal
 		params.attacker:HealEvent(flHeal, self:GetAbility(), params.attacker)
 	end
