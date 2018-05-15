aea6db3bc228570369442c3a7a4d058e3f23a532
Yahnich <y.slagmeulder@hotmail.com>
2018-05-16 01:42:07 +0200

    necro abilities

--- game/scripts/vscripts/bosses/boss_necro/boss_necro_deathbringer.lua
+++ game/scripts/vscripts/bosses/boss_necro/boss_necro_deathbringer.lua
@@ -1,1 +1,9 @@
 boss_necro_deathbringer = class({})
+
+function boss_necro_deathbringer:GetIntrinsicModifierName()
+	return "modifier_boss_necro_deathbringer"
+end
+
+modifier_boss_necro_deathbringer = class({})
+LinkLuaModifier("modifier_boss_necro_deathbringer", "bosses/boss_necro/boss_necro_deathbringer", LUA_MODIFIER_MOTION_NONE )
+
--- game/scripts/vscripts/bosses/boss_necro/boss_necro_take_the_weak.lua
+++ game/scripts/vscripts/bosses/boss_necro/boss_necro_take_the_weak.lua
@@ -1,1 +1,18 @@
 boss_necro_take_the_weak = class({})
+
+function boss_necro_take_the_weak:GetIntrinsicModifierName()
+	return "modifier_boss_necro_take_the_weak"
+end
+
+modifier_boss_necro_take_the_weak = class({})
+LinkLuaModifier("modifier_boss_necro_take_the_weak", "bosses/boss_necro/boss_necro_take_the_weak", LUA_MODIFIER_MOTION_NONE)
+
+function modifier_boss_necro_take_the_weak:DeclareFunctions()
+	return {MODIFIER_EVENT_ON_TAKEDAMAGE}
+end
+
+function modifier_boss_necro_take_the_weak:OnTakeDamage(params)
+	if params.attacker == self:GetParent() and params.unit ~= self:GetParent() and not params.unit:IsRealHero() and not HasBit(params.damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) then
+		params.unit:AttemptKill(self:GetAbility(), params.attacker)
+	end
+end
