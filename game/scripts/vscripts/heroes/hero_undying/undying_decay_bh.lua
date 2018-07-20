ef0ca018046a0fd087ce234ac70e3c2da101e6c6
Yahnich <y.slagmeulder@hotmail.com>
2018-07-20 17:59:48 +0200

    more undying

--- game/scripts/vscripts/heroes/hero_undying/undying_decay_bh.lua
+++ game/scripts/vscripts/heroes/hero_undying/undying_decay_bh.lua
@@ -1,4 +1,48 @@
 undying_decay_bh = class({})
 
+function undying_decay_bh:GetAOERadius()
+	return self:GetTalentSpecialValueFor("radius")
+end
+
 function undying_decay_bh:OnSpellStart()
+	local caster = self:GetCaster()
+	local position = self:GetCursorPosition()
+	
+	
+	local damage = self:GetTalentSpecialValueFor("damage")
+	local duration = self:GetTalentSpecialValueFor("duration")
+	local radius = self:GetTalentSpecialValueFor("radius")
+	local bossStr = TernaryOperator( self:GetTalentSpecialValueFor("str_per_boss"), caster:HasScepter(), self:GetTalentSpecialValueFor("scepter_str_per_boss") )
+	local mobStr = self:GetTalentSpecialValueFor("str_per_mob")
+	for _, enemy in ipairs( caster:FindEnemyUnitsInRadius( position, radius) ) do
+		local str = TernaryOperator( bossStr, enemy:IsRoundBoss(), mobStr )
+		for i = 1, str do
+			caster:AddNewModifier(caster, self, "modifier_undying_decay_bh", {duration = duration})
+		end
+		self:DealDamage( caster, enemy, damage )
+	end
+	
+	ParticleManager:FireParticle(pEffect, PATTACH_WORLDORIGIN, nil, {[0] = position})
+	EmitSoundOnLocationWithCaster( position, sEffect, caster )
+end
+
+modifier_undying_decay_bh = class({})
+LinkLuaModifier("modifier_undying_decay_bh", "heroes/hero_undying/undying_decay_bh", LUA_MODIFIER_MOTION_NONE)
+
+if IsServer() then
+	function modifier_undying_decay_bh:OnCreated()
+		self:AddIndependentStack( self:GetRemainingTime() )
+	end
+	
+	function modifier_undying_decay_bh:OnRefresh()
+		self:AddIndependentStack( self:GetRemainingTime() )
+	end
+end
+
+function modifier_undying_decay_bh:DeclareFunctions()
+	return {MODIFIER_PROPERTY_STRENGTH_BONUS}
+end
+
+function modifier_undying_decay_bh:GetStrength()
+	return self:GetStackCount()
 end
--- game/scripts/vscripts/heroes/hero_undying/undying_flesh_golem_bh.lua
+++ game/scripts/vscripts/heroes/hero_undying/undying_flesh_golem_bh.lua
@@ -1,5 +1,9 @@
 undying_flesh_golem_bh = class({})
 
+function undying_flesh_golem_bh:GetCastRange(target, position)
+	return self:GetTalentSpecialValueFor("radius")
+end
+
 function undying_flesh_golem_bh:OnSpellStart()
 	local caster = self:GetCaster()
 	caster:AddNewModifier(caster, self, "modifier_undying_flesh_golem_bh", {duration = self:GetTalentSpecialValueFor("duration")})
@@ -8,7 +12,7 @@ end
 modifier_undying_flesh_golem_bh = class({})
 LinkLuaModifier("modifier_undying_flesh_golem_bh", "heroes/hero_undying/undying_flesh_golem_bh", LUA_MODIFIER_MOTION_NONE)
 
-function modifier_undying_flesh_golem_bh:Oncreated()
+function modifier_undying_flesh_golem_bh:OnCreated()
 	self.radius = self:GetTalentSpecialValueFor("radius")
 	self.bHeal = self:GetTalentSpecialValueFor("death_heal") / 100
 	self.mHeal = self:GetTalentSpecialValueFor("death_heal_creep") / 100
@@ -31,13 +35,23 @@ function modifier_undying_flesh_golem_bh:OnDeath(params)
 	end
 end
 
+function modifier_undying_flesh_golem_bh_aura:DeclareFunctions()
+	return {MODIFIER_EVENT_ON_ATTACK_LANDED}
+end
+
+function modifier_undying_flesh_golem_bh:OnAttackLanded(params)
+	if params.unit == self:GetParent() and self:GetParent():HasTalent("special_bonus_unique_undying_flesh_golem_2") then
+		if self:RollPRNG( self:GetParent():FindTalentValue("special_bonus_unique_undying_flesh_golem_2") ) then
+		end
+	end
+end
 
 function modifier_undying_flesh_golem_bh:IsAura()
 	return true
 end
 
 function modifier_undying_flesh_golem_bh:GetModifierAura()
-	return "modifier_alchemist_acid_spray_ebf_debuff"
+	return "modifier_undying_flesh_golem_bh_aura"
 end
 
 function modifier_undying_flesh_golem_bh:GetAuraRadius()
@@ -58,4 +72,50 @@ end
 
 function modifier_undying_flesh_golem_bh:GetAuraSearchFlags()    
 	return DOTA_UNIT_TARGET_FLAG_NONE
+end
+
+modifier_undying_flesh_golem_bh_aura = class({})
+LinkLuaModifier("modifier_undying_flesh_golem_bh_aura", "heroes/hero_undying/undying_flesh_golem_bh", LUA_MODIFIER_MOTION_NONE)
+
+function modifier_undying_flesh_golem_bh_aura:OnCreated()
+	self.min_dmg = self:GetTalentSpecialValueFor("max_damage_amp")
+	self.max_dmg = self:GetTalentSpecialValueFor("min_damage_amp")
+	self.min_slow = self:GetTalentSpecialValueFor("max_speed_slow")
+	self.max_slow = self:GetTalentSpecialValueFor("min_speed_slow")
+	self.min_radius = self:GetTalentSpecialValueFor("full_power_radius")
+	self.max_radius = self:GetTalentSpecialValueFor("radius")
+	if IsServer() then
+		self:StartIntervalThink(0.5)
+	end
+end
+
+function modifier_undying_flesh_golem_bh_aura:OnRefresh()
+	self.min_dmg = self:GetTalentSpecialValueFor("max_damage_amp")
+	self.max_dmg = self:GetTalentSpecialValueFor("min_damage_amp")
+	self.min_slow = self:GetTalentSpecialValueFor("max_speed_slow")
+	self.max_slow = self:GetTalentSpecialValueFor("min_speed_slow")
+	self.min_radius = self:GetTalentSpecialValueFor("radius")
+	self.max_radius = self:GetTalentSpecialValueFor("full_power_radius")
+end
+
+function modifier_undying_flesh_golem_bh_aura:OnIntervalThink()
+	local distance = CalculateDistance( self:GetParent(), self:GetCaster() )
+	
+	local boundRange = self.min_radius - self.max_radius
+	local distPct = math.ceil( (self.min_radius - distance) / boundRange ) * 100
+	self:SetStackcount(distPct)
+end
+
+function modifier_undying_flesh_golem_bh_aura:DeclareFunctions()
+	return {MODIFIER_PROPERTY_MOVESPEED,
+			MODIFIER_PROPERTY_INcOMING_DAMAGE,
+			MODIFIER_EVENT_ON_ATTAcK_LANDED}
+end
+
+function modifier_undying_flesh_golem_bh_aura:GetMS()
+	return math.max( self.min_slow, math.min( self.max_slow, self:GetStackount()/100 * self.max_slow ) )
+end
+
+function modifier_undying_flesh_golem_bh_aura:GetMS()
+	return math.max( self.min_dmg, math.min( self.max_dmg, self:GetStackount()/100 * self.max_dmg ) )
 end
