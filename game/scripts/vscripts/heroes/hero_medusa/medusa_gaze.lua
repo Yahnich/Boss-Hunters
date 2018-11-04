281d82a028760f3638e5265e5a5f23ec502c3270
Sidearms <gregradi@yahoo.com>
2018-11-04 16:49:32 -0600

    Medusa Rework

---
+++ game/scripts/vscripts/heroes/hero_medusa/medusa_gaze.lua
@@ -0,0 +1,286 @@
+medusa_gaze = class({})
+LinkLuaModifier("modifier_medusa_gaze", "heroes/hero_medusa/medusa_gaze", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_medusa_gaze_slow", "heroes/hero_medusa/medusa_gaze", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_medusa_gaze_stun", "heroes/hero_medusa/medusa_gaze", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_medusa_gaze_stun_lesser", "heroes/hero_medusa/medusa_gaze", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_stunned_generic", "libraries/modifiers/modifier_stunned_generic", LUA_MODIFIER_MOTION_NONE)
+
+function medusa_gaze:IsStealable()
+	return true
+end
+
+function medusa_gaze:IsHiddenWhenStolen()
+	return false
+end
+
+function medusa_gaze:OnSpellStart()
+	local caster = self:GetCaster()
+
+	EmitSoundOn("Hero_Medusa.StoneGaze.Cast", caster)
+	caster:AddNewModifier(caster, self, "modifier_medusa_gaze", {Duration = self:GetTalentSpecialValueFor("duration")})
+end
+
+modifier_medusa_gaze = class({})
+
+function modifier_medusa_gaze:OnCreated(table)
+	if IsServer() then
+		local caster = self:GetParent()
+		caster:StartGesture(ACT_DOTA_MEDUSA_STONE_GAZE)
+
+		local nfx = ParticleManager:CreateParticle("particles/units/heroes/hero_medusa/medusa_stone_gaze_active.vpcf", PATTACH_POINT_FOLLOW, caster)
+					ParticleManager:SetParticleControlEnt(nfx, 0, caster, PATTACH_POINT_FOLLOW, "attach_head", caster:GetAbsOrigin(), true)
+					ParticleManager:SetParticleControlEnt(nfx, 1, caster, PATTACH_POINT_FOLLOW, "attach_head", caster:GetAbsOrigin(), true)
+		self:AttachEffect(nfx)
+
+		self.radius = self:GetTalentSpecialValueFor("radius")
+		self.length = self:GetTalentSpecialValueFor("length")
+		self.duration = self:GetTalentSpecialValueFor("face_duration")
+
+		self:StartIntervalThink(0.5)
+	end
+end
+
+function modifier_medusa_gaze:OnIntervalThink()
+	local caster = self:GetParent()
+
+	local x = 25 ---Adds 25 stacks every 0.5 seconds equals 100 at 2 seconds.
+
+	local enemies = caster:FindEnemyUnitsInCone(caster:GetForwardVector(), caster:GetAbsOrigin(), self.radius, self.length, {})
+	for _,enemy in pairs(enemies) do
+		for i=1,x do
+			if not enemy:HasModifier("modifier_medusa_gaze_stun") then
+				enemy:AddNewModifier(caster, self:GetAbility(), "modifier_medusa_gaze_slow", {Duration = self.duration}):IncrementStackCount()
+			end
+		end
+
+		if caster:HasTalent("special_bonus_unique_medusa_gaze_2") then
+			local damage = caster:GetMaxMana() * 1/100
+			self:GetAbility():DealDamage(caster, enemy, damage * 0.5, {}, OVERHEAD_ALERT_BONUS_POISON_DAMAGE)
+		end
+	end
+end
+
+function modifier_medusa_gaze:OnRemoved()
+	if IsServer() then
+		local caster = self:GetCaster()
+		caster:RemoveGesture(ACT_DOTA_MEDUSA_STONE_GAZE)
+	end
+end
+
+modifier_medusa_gaze_slow = class({})
+function modifier_medusa_gaze_slow:OnCreated(table)
+	if IsServer() then
+		local parent = self:GetParent()
+		local caster = self:GetCaster()
+
+		EmitSoundOn("Hero_Medusa.StoneGaze.Target", parent)
+
+		local nfx = ParticleManager:CreateParticle("particles/units/heroes/hero_medusa/medusa_stone_gaze_facing.vpcf", PATTACH_POINT_FOLLOW, caster)
+					ParticleManager:SetParticleControlEnt(nfx, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent:GetAbsOrigin(), true)
+					ParticleManager:SetParticleControlEnt(nfx, 1, caster, PATTACH_POINT_FOLLOW, "attach_head", caster:GetAbsOrigin(), true)
+		self:AttachEffect(nfx)
+
+		local nfx2 = ParticleManager:CreateParticle("particles/units/heroes/hero_medusa/medusa_stone_gaze_debuff.vpcf", PATTACH_POINT_FOLLOW, caster)
+					ParticleManager:SetParticleControlEnt(nfx2, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent:GetAbsOrigin(), true)
+					ParticleManager:SetParticleControlEnt(nfx2, 1, caster, PATTACH_POINT_FOLLOW, "attach_head", caster:GetAbsOrigin(), true)
+		self:AttachEffect(nfx2)
+
+		if self:GetStackCount() > 99 then
+			self:GetParent():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_medusa_gaze_stun", {Duration = self:GetTalentSpecialValueFor("stone_duration")})
+			self:Destroy()
+		end
+		self:StartIntervalThink(0.33)
+	end
+end
+
+function modifier_medusa_gaze_slow:OnRefresh(table)
+	if IsServer() then
+		if self:GetStackCount() > 99 then
+			self:GetParent():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_medusa_gaze_stun", {Duration = self:GetTalentSpecialValueFor("stone_duration")})
+			self:Destroy()
+		end
+	end
+end
+
+function modifier_medusa_gaze_slow:OnIntervalThink()
+	if IsServer() then
+		if self:GetStackCount() > 99 then
+			self:GetParent():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_medusa_gaze_stun", {Duration = self:GetTalentSpecialValueFor("stone_duration")})
+			self:Destroy()
+		end
+	end
+end
+
+function modifier_medusa_gaze_slow:GetTexture()
+	return "ancient_apparition_cold_feet"
+end
+
+function modifier_medusa_gaze_slow:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
+		MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+		MODIFIER_PROPERTY_TURN_RATE_PERCENTAGE
+	}
+	return funcs
+end
+
+function modifier_medusa_gaze_slow:GetModifierMoveSpeedBonus_Percentage()
+	return -1 * self:GetStackCount()
+end
+
+function modifier_medusa_gaze_slow:GetModifierAttackSpeedBonus_Constant()
+	return -1 * self:GetStackCount()
+end
+
+function modifier_medusa_gaze_slow:GetModifierTurnRate_Percentage()
+	return -1 * self:GetStackCount()
+end
+
+function modifier_medusa_gaze_slow:IsPurgable()
+	return false
+end
+
+function modifier_medusa_gaze_slow:IsDebuff()
+	return true
+end
+
+modifier_medusa_gaze_stun = class({})
+
+function modifier_medusa_gaze_stun:OnCreated(table)
+	self.bonusPhys = self:GetTalentSpecialValueFor("bonus_physical")
+
+	local caster = self:GetCaster()
+	if caster:HasTalent("special_bonus_unique_medusa_gaze_1") then
+		self.mr = self:GetTalentSpecialValueFor("mr_reduc")
+	end
+
+	if IsServer() then
+		local parent = self:GetParent()
+
+		EmitSoundOn("Hero_Medusa.StoneGaze.Stun", parent)
+
+		local nfx = ParticleManager:CreateParticle("particles/units/heroes/hero_medusa/medusa_stone_gaze_debuff_stoned.vpcf", PATTACH_POINT_FOLLOW, caster)
+					ParticleManager:SetParticleControlEnt(nfx, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent:GetAbsOrigin(), true)
+					ParticleManager:SetParticleControlEnt(nfx, 1, caster, PATTACH_POINT_FOLLOW, "attach_head", caster:GetAbsOrigin(), true)
+		self:AttachEffect(nfx)
+	end
+end
+
+function modifier_medusa_gaze_stun:OnRefresh(table)
+	local caster = self:GetCaster()
+	if caster:HasTalent("special_bonus_unique_medusa_gaze_1") then
+		self.mr = self:GetTalentSpecialValueFor("mr_reduc")
+	end
+
+	self.bonusPhys = self:GetTalentSpecialValueFor("bonus_physical")
+end
+
+function modifier_medusa_gaze_stun:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_INCOMING_PHYSICAL_DAMAGE_PERCENTAGE,
+		MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS
+	}
+	return funcs
+end
+
+function modifier_medusa_gaze_stun:GetModifierIncomingPhysicalDamage_Percentage()
+	return self.bonusPhys
+end
+
+function modifier_medusa_gaze_stun:GetModifierMagicalResistanceBonus()
+	return self.mr
+end
+
+function modifier_medusa_gaze_stun:CheckState()
+	local state = { [MODIFIER_STATE_STUNNED] = true,
+					[MODIFIER_STATE_FROZEN] = true}
+	return state
+end
+
+function modifier_medusa_gaze_stun:IsPurgable()
+	return true
+end
+
+function modifier_medusa_gaze_stun:IsDebuff()
+	return true
+end
+
+function modifier_medusa_gaze_stun:GetStatusEffectName()
+	return "particles/status_fx/status_effect_medusa_stone_gaze.vpcf"
+end
+
+function modifier_medusa_gaze_stun:StatusEffectPriority()
+	return 11
+end
+
+modifier_medusa_gaze_stun_lesser = class({modifier_stunned_generic})
+
+function modifier_medusa_gaze_stun_lesser:OnCreated(table)
+	self.bonusPhys = self:GetTalentSpecialValueFor("bonus_physical")
+
+	local caster = self:GetCaster()
+	if caster:HasTalent("special_bonus_unique_medusa_gaze_1") then
+		self.mr = self:GetTalentSpecialValueFor("mr_reduc")
+	end
+
+	if IsServer() then
+		local parent = self:GetParent()
+
+		EmitSoundOn("Hero_Medusa.StoneGaze.Stun", parent)
+
+		local nfx = ParticleManager:CreateParticle("particles/units/heroes/hero_medusa/medusa_stone_gaze_debuff_stoned.vpcf", PATTACH_POINT_FOLLOW, caster)
+					ParticleManager:SetParticleControlEnt(nfx, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent:GetAbsOrigin(), true)
+					ParticleManager:SetParticleControlEnt(nfx, 1, caster, PATTACH_POINT_FOLLOW, "attach_head", caster:GetAbsOrigin(), true)
+		self:AttachEffect(nfx)
+	end
+end
+
+function modifier_medusa_gaze_stun_lesser:OnRefresh(table)
+	local caster = self:GetCaster()
+	if caster:HasTalent("special_bonus_unique_medusa_gaze_1") then
+		self.mr = self:GetTalentSpecialValueFor("mr_reduc")
+	end
+
+	self.bonusPhys = self:GetTalentSpecialValueFor("bonus_physical")
+end
+
+function modifier_medusa_gaze_stun_lesser:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_INCOMING_PHYSICAL_DAMAGE_PERCENTAGE,
+		MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS
+	}
+	return funcs
+end
+
+function modifier_medusa_gaze_stun_lesser:GetModifierIncomingPhysicalDamage_Percentage()
+	return self.bonusPhys
+end
+
+function modifier_medusa_gaze_stun_lesser:GetModifierMagicalResistanceBonus()
+	return self.mr
+end
+
+function modifier_medusa_gaze_stun_lesser:CheckState()
+	local state = {[MODIFIER_STATE_FROZEN] = true}
+	return state
+end
+
+function modifier_medusa_gaze_stun_lesser:IsPurgable()
+	return true
+end
+
+function modifier_medusa_gaze_stun_lesser:IsDebuff()
+	return true
+end
+
+function modifier_medusa_gaze_stun_lesser:GetStatusEffectName()
+	return "particles/status_fx/status_effect_medusa_stone_gaze.vpcf"
+end
+
+function modifier_medusa_gaze_stun_lesser:StatusEffectPriority()
+	return 11
+end
+
+function modifier_medusa_gaze_stun_lesser:GetTexture()
+	return "medusa_stone_gaze"
+end
---
+++ game/scripts/vscripts/heroes/hero_medusa/medusa_viper.lua
@@ -0,0 +1,161 @@
+medusa_viper = class({})
+LinkLuaModifier("modifier_medusa_viper", "heroes/hero_medusa/medusa_viper", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_medusa_viper_dot", "heroes/hero_medusa/medusa_viper", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_medusa_gaze_stun_lesser", "heroes/hero_medusa/medusa_gaze", LUA_MODIFIER_MOTION_NONE)
+
+function medusa_viper:IsStealable()
+	return false
+end
+
+function medusa_viper:IsHiddenWhenStolen()
+	return false
+end
+
+function medusa_viper:GetCastRange(vLocation, hTarget)
+	return self:GetCaster():GetAttackRange()
+end
+
+function medusa_viper:OnSpellStart()
+	local caster = self:GetCaster()
+
+	if caster:HasModifier("modifier_medusa_viper") then
+		EmitSoundOn("Hero_Medusa.MysticSnake.Cast", caster)
+
+		self:RefundManaCost()
+		local stacks = caster:FindModifierByName("modifier_medusa_viper"):GetStackCount()
+		for i=1,stacks do
+			self:FireLinearArrow()
+		end
+		caster:RemoveModifierByName("modifier_medusa_viper")
+	else
+		caster:AddNewModifier(caster, self, "modifier_medusa_viper", {Duration = 10}):SetStackCount(self:GetTalentSpecialValueFor("arrow_count"))
+		self:EndCooldown()
+	end
+end
+
+function medusa_viper:OnProjectileHit(hTarget, vLocation)
+	local caster = self:GetCaster()
+	local damage = self:GetTalentSpecialValueFor("damage_mod")
+
+	EmitSoundOnLocationWithCaster(vLocation, "Hero_Medusa.MysticSnake.Target", caster)
+
+	if hTarget then
+		if caster:HasTalent("special_bonus_unique_medusa_viper_1") then
+			local duration = caster:FindTalentValue("special_bonus_unique_medusa_viper_1")
+			hTarget:Break(self, caster, duration, false)
+		end
+
+		if caster:HasTalent("special_bonus_unique_medusa_viper_2") then
+			local ability = caster:FindAbilityByName("medusa_gaze")
+			hTarget:AddNewModifier(caster, self, "modifier_medusa_gaze_stun_lesser", {Duration = 1})
+		end
+
+		hTarget:AddNewModifier(caster, self, "modifier_medusa_viper_dot", {Duration = self:GetTalentSpecialValueFor("duration")})
+		return true
+	end
+end
+
+function medusa_viper:FireLinearArrow()
+	local caster = self:GetCaster()
+	local fDir = caster:GetForwardVector()
+	local spread = self:GetTalentSpecialValueFor("cone_spread")
+	local rndAng = math.rad(RandomInt(-spread/2, spread/2))
+	local dirX = fDir.x * math.cos(rndAng) - fDir.y * math.sin(rndAng); 
+	local dirY = fDir.x * math.sin(rndAng) + fDir.y * math.cos(rndAng);
+	local direction = Vector( dirX, dirY, 0 )
+
+	local speed = caster:GetProjectileSpeed()
+	local vel = direction * speed
+	local distance = caster:GetAttackRange() + 100
+	local width = self:GetTalentSpecialValueFor("width")
+
+	local position = caster:GetAbsOrigin() + caster:GetForwardVector()*50 + Vector(0,0,150)
+	self:FireLinearProjectile("particles/units/heroes/hero_medusa/medusa_viper.vpcf", vel, distance, width, {origin = position}, true, false, 0)
+end
+
+modifier_medusa_viper = class({})
+
+function modifier_medusa_viper:DeclareFunctions()
+    local funcs = {
+        MODIFIER_EVENT_ON_ATTACK,
+        MODIFIER_PROPERTY_TRANSLATE_ACTIVITY_MODIFIERS
+    }
+
+    return funcs
+end
+
+function modifier_medusa_viper:OnAttack(params)
+	if IsServer() then
+		local caster = self:GetCaster()
+		local target = params.target
+		local attacker = params.attacker
+
+		if caster == attacker then
+			if not caster:IsInAbilityAttackMode() then
+				EmitSoundOn("Hero_Medusa.MysticSnake.Cast", caster)
+				local enemies = caster:FindEnemyUnitsInRadius(caster:GetAbsOrigin(), caster:GetAttackRange())
+				for _,enemy in pairs(enemies) do
+					self:GetAbility():FireTrackingProjectile("particles/units/heroes/hero_medusa/medusa_mystic_snake_projectile.vpcf", enemy, caster:GetProjectileSpeed(), {}, DOTA_PROJECTILE_ATTACHMENT_ATTACK_1, true, false, 0)
+					self:DecrementStackCount()
+					if self:GetStackCount() < 1 then
+						self:Destroy()
+					end
+					break
+				end
+			end
+		end
+	end
+end
+
+function modifier_medusa_viper:GetActivityTranslationModifiers()
+	return "split_shot"
+end
+
+function modifier_medusa_viper:IsHidden()
+	return false
+end
+
+modifier_medusa_viper_dot = class({})
+
+function modifier_medusa_viper_dot:OnCreated(table)
+	self.slow = self:GetTalentSpecialValueFor("move_slow")
+
+	if IsServer then
+		self.dot = self:GetTalentSpecialValueFor("damage")
+		self:StartIntervalThink(1)
+	end
+end
+
+function modifier_medusa_viper_dot:OnRefresh(table)
+	self.slow = self:GetTalentSpecialValueFor("move_slow")
+
+	if IsServer then
+		self.dot = self:GetTalentSpecialValueFor("damage")
+	end
+end
+
+function modifier_medusa_viper_dot:OnIntervalThink()
+	if IsServer() then
+		local caster = self:GetCaster()
+		local parent = self:GetParent()
+		local ability = self:GetAbility()
+
+		ability:DealDamage(caster, parent, self.dot, {}, OVERHEAD_ALERT_BONUS_POISON_DAMAGE)
+	end
+end
+
+function modifier_medusa_viper_dot:DeclareFunctions()
+    local funcs = {
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE
+    }
+
+    return funcs
+end
+
+function modifier_medusa_viper_dot:GetModifierMoveSpeedBonus_Percentage()
+	return self.slow
+end
+
+function modifier_medusa_viper_dot:GetEffectName()
+	return "particles/units/heroes/hero_venomancer/venomancer_poison_debuff_nova.vpcf"
+end
